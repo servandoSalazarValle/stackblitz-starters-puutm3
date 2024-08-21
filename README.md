ad09d99e4799f680aab9e6616956bbc67e016e50
servandoSalazarValle <servando.salazar@spaymentservices.com>
2024-08-21 13:22:14 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-puutm3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/servandoSalazarValle/stackblitz-starters-puutm3)
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+/*
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
+*/
+
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
